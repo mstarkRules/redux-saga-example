ed63693eadfd2a5d7e2f5733b3f2b0f0553af669
Marcos Amorim <markstm10@gmail.com>
2021-11-01 18:02:21 -0300

    🛠 update saga with try catch

--- src/store/sagas.js
+++ src/store/sagas.js
@@ -5,20 +5,27 @@ import {
   put,
   all,
   delay,
+  select,
 } from "redux-saga/effects";
 
-function apiGet(text) {
+function apiGet(text, length) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(text + " topissimo");
+      resolve(text + " topissimo" + length);
     }, 2000);
   });
 }
 
 function* asyncAddTodo(action) {
-  const response = yield call(apiGet, action.payload.text);
+  try {
+    const todos = yield select((state) => state.todos);
 
-  yield put({ type: "ADD_TODO", payload: { text: response } });
+    const response = yield call(apiGet, action.payload.text, todos.length);
+
+    yield put({ type: "ADD_TODO", payload: { text: response } });
+  } catch (err) {
+    yield put({ type: "ERROR" });
+  }
 }
 
 export default function* root() {
