86405033f70a02a2e24a768a40f024bc033bf2ec
Marcos Amorim <markstm10@gmail.com>
2021-11-01 16:24:58 -0300

    🛠 config sagamiddleware in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,15 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
 
 import todos from "./reducer";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   combineReducers({
     todos,
-  })
+  }),
+  applyMiddleware(sagaMiddleware)
 );
 
 export default store;
